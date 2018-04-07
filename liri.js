3f08ed5e7bfe482786467fcf22522b2dc6ec6a01
Erin <erinlbritton@gmail.com>
2018-04-07 18:50:53 -0400

    Added functions to read and write to text files

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ var Twitter = require('twitter');
 var request = require('request');
 var figlet = require('figlet');
 var moment = require('moment');
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
@@ -18,6 +19,11 @@ if (process.argv.length > 3) {
     } 
     criteria += process.argv.pop(); 
 }
+var nodeEntry = `${command} ${criteria}`;
+var nodeTime = moment().format("ddd, MMM D, YYYY");
+    nodeTime += " at ";
+    nodeTime += moment().format("hh:mm:ss a");
+var textFile = "log.txt";
 
 function spotifySearch(song) {
     spotify.search({ type: 'track', query: song, limit: 5 }, function(err, data) {
@@ -29,14 +35,21 @@ function spotifySearch(song) {
 
         figlet(spotifyResult.artists[0].name, {font: 'standard'}, function(err, data) {
             if (err) {
-                console.log(`• Artist:      ${spotifyResult.artists[0].name}`);
+                console.log(`• Artist:          ${spotifyResult.artists[0].name}`);
                 console.dir(err);
                 return;
             }
-            console.log(data);
-            console.log(`• Song:        ${spotifyResult.name}`);
-            console.log(`• Album:       ${spotifyResult.album.name}`);
-            console.log(`• Preview URL: ${spotifyResult.preview_url}`);
+            var output = 
+`Command "${nodeEntry}" executed at ${nodeTime}
+${data}
+
+• Song:            ${spotifyResult.name}
+• Album:           ${spotifyResult.album.name}
+• Preview URL:     ${spotifyResult.preview_url}
+
+`;
+            console.log(output);
+            logOutput(output);
         });    
         
     });
@@ -53,17 +66,27 @@ function twitterSearch() {
                     console.dir(err);
                     return;
                 }
-                console.log(data);
-                console.log("     ");
+                var output =
+`Command "${nodeEntry}" executed at ${nodeTime}
+${data}
+
+`;
                 for (var i = 0; i < tweets.length; i++) {
-                    console.log(`${tweets[i].text}`);
+                output +=
+`• Tweet #${i+1}
+${tweets[i].text}
+`;
                     var RT_at = tweets[i].created_at.split(" ");
                     var RT = `${RT_at[0]}, ${RT_at[1]} ${RT_at[2]}, ${RT_at[5]} at ${moment(RT_at[3], 'hh:mm:ss').format('h:mm:ss a')}`;
                     var created_at = tweets[i].retweeted_status.created_at.split(" ");
                     var created = `${created_at[0]}, ${created_at[1]} ${created_at[2]}, ${created_at[5]} at ${moment(created_at[3], 'hh:mm:ss').format('h:mm:ss a')}`;
-                    console.log(`RT: ${RT} | Originally Posted: ${created}`);
-                    console.log("     ");
-                }
+                output +=    
+`RT: ${RT} | Originally Posted: ${created}
+
+`; 
+                }   
+                console.log(output);
+                logOutput(output);
             });     
         } else {
             console.log(error);
@@ -77,20 +100,74 @@ function OMDBSearch(movie) {
         var body = JSON.parse(body);
         figlet(`${body.Title}`, {font: 'standard'}, function(err, data) {
             if (err) {
-                console.log(`Title: ${body.Title}`);
-                console.dir(err);
+                console.log(err);
                 return;
             }
-            console.log(data);
-            console.log(`• Year:            ${body.Year}`);
-            console.log(`• IMDB:            ${body.Ratings[0].Value}`);
-            console.log(`• Rotten Tomatoes: ${body.Ratings[1].Value}`);
-            console.log(`• Country:         ${body.Country}`);
-            console.log(`• Language(s):     ${body.Language}`);
-            console.log(`• Plot:            ${body.Plot}`);
-            console.log(`• Actors:          ${body.Actors}`);
+            var output =
+`Command "${nodeEntry}" executed at ${nodeTime}
+${data}
+
+• Year:            ${body.Year}
+• IMDB:            ${body.Ratings[0].Value}
+• Rotten Tomatoes: ${body.Ratings[1].Value}
+• Country:         ${body.Country}
+• Language(s):     ${body.Language}
+• Plot:            ${body.Plot}
+• Actors:          ${body.Actors}
+
+`;
+            console.log(output);
+            logOutput(output);
         });
     });
+};
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+        return console.log(error);
+        }
+    
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+        var command = dataArr[0];
+        var criteria = dataArr[1].split("\"").join("");
+    
+        // We will then re-display the content as an array for later use.
+        if (command === "spotify-this-song") {
+            if (criteria.length === 0) {
+                spotifySearch("The Sign Ace of Base");
+            } else {
+                spotifySearch(criteria);
+            }
+        } else if (command === "my-tweets") {
+            twitterSearch();
+        } else if (command === "movie-this") {
+            if (criteria.length === 0) {
+                OMDBSearch("Mr. Nobody");
+            } else {
+                OMDBSearch(criteria);
+            }
+        }
+    });
+};
+
+function logOutput(output) {
+    fs.appendFile(textFile, output, function(err) {
+
+        // If an error was experienced we say it.
+        if (err) {
+          console.log(err);
+        }
+      
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+          console.log(`Output saved to ${textFile}`);
+        }
+      
+      });
 };
 
 if (command === "spotify-this-song") {
@@ -107,4 +184,6 @@ if (command === "spotify-this-song") {
     } else {
         OMDBSearch(criteria);
     }
-}
+} else if (command === "do-what-it-says") {
+    doWhatItSays();
+};
